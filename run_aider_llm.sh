1fdb0d6151c2c07468fc3e9e940e4c276b40c642
Luis Gonzalez <decaturdev@gmail.com>
2023-07-24 07:52:52 +0100

    WIP Test

---
+++ run_aider_llm.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+OPENAI_API_BASE=https://streets-neighbor-for-adaptive.trycloudflare.com/v1
+~/Documents/git/aider-llama2/launch_free_aider --openai-api-base=https://streets-neighbor-for-adaptive.trycloudflare.com/v1 --openai-api-key=dummy --model=gpt-3.5-turbo
